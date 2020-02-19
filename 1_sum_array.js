03698ab1f18b8b6efc2085e164afde540984b11b
Tash Dutton <tashdutton@gmail.com>
2020-02-19 13:07:02 -0800

    SolvedAlgos

--- 1_sum_array.js
+++ 1_sum_array.js
@@ -3,3 +3,13 @@
 // When you have solved this, create new values for your array and ensure it still works as expected.
 
 // YOUR CODE HERE
+var arr=[6,3,5,1,2,4]
+var sum=0
+    for(var i=0; i < arr.length; i++){
+        
+        
+        console.log("num"+arr[i]);
+        sum=sum+arr[i];
+        console.log("sum"+sum);
+        
+    }
